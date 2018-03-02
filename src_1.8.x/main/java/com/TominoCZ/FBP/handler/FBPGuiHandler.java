446a79fe84cdfb5fb30b7261211d62aebbd0a3ad
Unknown <tom@dlnet.us>
2018-03-02 02:43:59 +0100

    V2.3.7
    1.8.x - soon

---
+++ src_1.8.x/main/java/com/TominoCZ/FBP/handler/FBPGuiHandler.java
@@ -0,0 +1,28 @@
+package com.TominoCZ.FBP.handler;
+
+import com.TominoCZ.FBP.FBP;
+import com.TominoCZ.FBP.gui.FBPGuiNote;
+
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class FBPGuiHandler {
+	@SubscribeEvent
+	public void onRenderGui(RenderGameOverlayEvent.Post evt) {
+		if (evt.type != ElementType.EXPERIENCE)
+			return;
+
+		if (FBP.frozen && FBP.isEnabled())
+			new FBPGuiNote();
+	}
+
+	@SubscribeEvent
+	public void onGuiMouseInput(GuiScreenEvent.MouseInputEvent e) {
+	}
+
+	@SubscribeEvent
+	public void onGuiKeyboardInput(GuiScreenEvent.KeyboardInputEvent e) {
+	}
+}
---
+++ src_1.8.x/main/java/com/TominoCZ/FBP/particle/IFBPShadedParticle.java
@@ -0,0 +1,8 @@
+package com.TominoCZ.FBP.particle;
+
+
+import net.minecraft.client.renderer.WorldRenderer;
+
+public interface IFBPShadedParticle {
+	void renderShadedParticle(WorldRenderer buf, float partialTicks);
+}
